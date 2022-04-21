a0af1f9e27b0e00cab3b3588cc94c1c43fc93086
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-21 22:15:46 +0200

    choice hooks and onClick event

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import { useState } from "react";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "/img/helmet-1.png" },
-  { src: "/img/potion-1.png" },
-  { src: "/img/ring-1.png" },
-  { src: "/img/scroll-1.png" },
-  { src: "/img/shield-1.png" },
-  { src: "/img/sword-1.png" },
+  { src: "./img/helmet-1.png" },
+  { src: "./img/potion-1.png" },
+  { src: "./img/ring-1.png" },
+  { src: "./img/scroll-1.png" },
+  { src: "./img/shield-1.png" },
+  { src: "./img/sword-1.png" },
 ];
 
 function App() {
   const [cards, setCards] = useState([]);
   const [turns, setTurns] = useState(0);
+  const [choiceOne, setChoiceOne] = useState(null);
+  const [choiceTwo, setChoiceTwo] = useState(null);
 
   // shuffle cards
   const shuffleCards = () => {
--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -5,7 +5,12 @@ export default function SingleCard({ card }) {
     <div className="card">
       <div>
         <img className="front" src={card.src} alt="card front" />
-        <img className="back" src="/img/cover.png" alt="card back" />
+        <img
+          className="back"
+          src="/img/cover.png"
+          // onClick={handleCLick}
+          alt="card back"
+        />
       </div>
     </div>
   );
