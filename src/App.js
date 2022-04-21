d7cf1a324000963f2b587a000fcbf4b68dc728d2
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-21 21:39:09 +0200

    mapping through cards

--- src/App.js
+++ src/App.js
@@ -33,7 +33,12 @@ function App() {
 
       <div className="card-grid">
         {cards.map((card) => (
-          <div className="card" key={card.id}></div>
+          <div className="card" key={card.id}>
+            <div>
+              <img className="front" src={card.src} alt="card front" />
+              <img className="back" src="/img/cover.png" alt="card back" />
+            </div>
+          </div>
         ))}
       </div>
     </div>
