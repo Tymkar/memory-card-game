040703de3b96e900bd219c2f360b3deced1ab427
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-22 09:20:17 +0200

    selected cards comparison

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import SingleCard from "./components/SingleCard";
 
 const cardImages = [
-  { src: "./img/helmet-1.png" },
-  { src: "./img/potion-1.png" },
-  { src: "./img/ring-1.png" },
-  { src: "./img/scroll-1.png" },
-  { src: "./img/shield-1.png" },
-  { src: "./img/sword-1.png" },
+  { src: "./img/helmet-1.png", matched: false },
+  { src: "./img/potion-1.png", matched: false },
+  { src: "./img/ring-1.png", matched: false },
+  { src: "./img/scroll-1.png", matched: false },
+  { src: "./img/shield-1.png", matched: false },
+  { src: "./img/sword-1.png", matched: false },
 ];
 
 function App() {
@@ -32,9 +32,38 @@ function App() {
     choiceOne ? setChoiceTwo(card) : setChoiceOne(card);
   };
 
+  // compare 2 selected cards
+  useEffect(() => {
+    if (choiceOne && choiceTwo) {
+      if (choiceOne.src === choiceTwo.src) {
+        setCards((prevCards) => {
+          return prevCards.map((card) => {
+            if (card.src === choiceOne.src) {
+              return { ...card, matched: true };
+            } else {
+              return card;
+            }
+          });
+        });
+        resetTurn();
+      } else {
+        resetTurn();
+      }
+    }
+  }, [choiceOne, choiceTwo]);
+
+  console.log(cards);
+
+  // reset choices and increase turns
+  const resetTurn = () => {
+    setChoiceOne(null);
+    setChoiceTwo(null);
+    setTurns((prevTurns) => prevTurns + 1);
+  };
+
   return (
     <div className="App">
-      <h1>Magic Memory</h1>
+      <h1>Memory Game</h1>
       <button onClick={shuffleCards}>New Game</button>
 
       <div className="card-grid">
