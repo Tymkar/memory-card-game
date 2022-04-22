e9ac1617c384af7f93373b100a8d1b3fad181c72
Tymoteusz Karczewski <tymkar2020@gmail.com>
2022-04-22 10:36:28 +0200

    cards flip

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,11 +1,11 @@
-export default function SingleCard({ card, handleChoice }) {
+export default function SingleCard({ card, handleChoice, flipped }) {
   const handleClick = () => {
     handleChoice(card);
   };
 
   return (
     <div className="card">
-      <div>
+      <div className={flipped ? "flipped" : ""}>
         <img className="front" src={card.src} alt="card front" />
         <img
           className="back"
